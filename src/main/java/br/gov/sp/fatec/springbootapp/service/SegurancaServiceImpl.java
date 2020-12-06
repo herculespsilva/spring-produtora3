e89c8b5dd4c803cad9322574db2a69e9ae68cf8b
herculespsilva <herculespsilva@hotmail.com>
2020-12-06 22:27:05 +0000

    alter

--- src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
@@ -23,80 +23,79 @@ import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 @Service("segurancaService")
 public class SegurancaServiceImpl implements SegurancaService {
 
-  @Autowired
-  private AutorizacaoRepository autRepo;
+    @Autowired
+    private AutorizacaoRepository autRepo;
 
-  @Autowired
-  private UsuarioRepository usuarioRepo;
+    @Autowired
+    private UsuarioRepository usuarioRepo;
 
-  @Autowired
-  private PasswordEncoder passEncoder;
+    @Autowired
+    private PasswordEncoder passEncoder;
 
-  @Transactional
-  public Usuario criarUsuario(String nome, String senha, String autorizacao) {
-    Autorizacao aut = autRepo.findByNome(autorizacao);
-    if (aut == null) {
-      aut = new Autorizacao();
-      aut.setNome(autorizacao);
-      autRepo.save(aut);
+    @Transactional
+    public Usuario criarUsuario(String nome, String senha, String autorizacao) {
+        Autorizacao aut = autRepo.findByNome(autorizacao);
+        if (aut == null) {
+            aut = new Autorizacao();
+            aut.setNome(autorizacao);
+            autRepo.save(aut);
+        }
+        Usuario usuario = new Usuario();
+        usuario.setNome(nome);
+        usuario.setSenha(passEncoder.encode(senha));
+        usuario.setAutorizacoes(new HashSet<Autorizacao>());
+        usuario.getAutorizacoes().add(aut);
+        usuarioRepo.save(usuario);
+        return usuario;
     }
-    Usuario usuario = new Usuario();
-    usuario.setNome(nome);
-    usuario.setSenha(passEncoder.encode(senha));
-    usuario.setAutorizacoes(new HashSet<Autorizacao>());
-    usuario.getAutorizacoes().add(aut);
-    usuarioRepo.save(usuario);
-    return usuario;
-  }
 
-  @Override
-  @PreAuthorize("hasRole('ADMIN')")
-  public List<Usuario> buscarTodosUsuarios() {
-    return usuarioRepo.findAll();
-  }
-
-  @Override
-  @PreAuthorize("hasAnyRole('ADMIN', 'USUARIO')")
-  public Usuario buscarUsuarioPorId(Long id) {
-    Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
-    if (usuarioOp.isPresent()) {
-      return usuarioOp.get();
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<Usuario> buscarTodosUsuarios() {
+        return usuarioRepo.findAll();
     }
-    throw new RegistroNaoEncontradoException("Usuário não encontrado!");
-  }
 
-  @Override
-  @PreAuthorize("isAuthenticated()")
-  public Usuario buscarUsuarioPorNome(String nome) {
-    Usuario usuario = usuarioRepo.findByNome(nome);
-    if (usuario != null) {
-      return usuario;
+    @Override
+    @PreAuthorize("hasAnyRole('ADMIN', 'USUARIO')")
+    public Usuario buscarUsuarioPorId(Long id) {
+        Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+        if (usuarioOp.isPresent()) {
+            return usuarioOp.get();
+        }
+        throw new RegistroNaoEncontradoException("Usuário não encontrado!");
     }
-    throw new RegistroNaoEncontradoException("Usuário não encontrado!");
 
-  }
+    @Override
+    @PreAuthorize("isAuthenticated()")
+    public Usuario buscarUsuarioPorNome(String nome) {
+        Usuario usuario = usuarioRepo.findByNome(nome);
+        if (usuario != null) {
+            return usuario;
+        }
+        throw new RegistroNaoEncontradoException("Usuário não encontrado!");
 
-  @Override
-  @PreAuthorize("isAuthenticated()")
-  public Autorizacao buscarAutorizacaoPorNome(String nome) {
-    Autorizacao autorizacao = autRepo.findByNome(nome);
-    if (autorizacao != null) {
-      return autorizacao;
     }
-    throw new RegistroNaoEncontradoException("Autorização não encontrada!");
-  }
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    Usuario usuario = usuarioRepo.findByNome(username);
-    if (usuario == null) {
-      throw new UsernameNotFoundException("Usuário " + username + " não encontrado!");
+    @Override
+    @PreAuthorize("isAuthenticated()")
+    public Autorizacao buscarAutorizacaoPorNome(String nome) {
+        Autorizacao autorizacao = autRepo.findByNome(nome);
+        if (autorizacao != null) {
+            return autorizacao;
+        }
+        throw new RegistroNaoEncontradoException("Autorização não encontrada!");
     }
-    return User.builder().username(username).password(usuario.getSenha())
-        .authorities(usuario.getAutorizacoes().stream()
-            .map(Autorizacao::getNome).collect(Collectors.toList())
-            .toArray(new String[usuario.getAutorizacoes().size()]))
-        .build();
-  }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepo.findByNome(username);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário " + username + " não encontrado!");
+        }
+        return User.builder().username(username).password(usuario.getSenha())
+                .authorities(usuario.getAutorizacoes().stream().map(Autorizacao::getNome).collect(Collectors.toList())
+                        .toArray(new String[usuario.getAutorizacoes().size()]))
+                .build();
+    }
 
 }
